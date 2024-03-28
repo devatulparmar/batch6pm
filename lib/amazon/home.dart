7b766a18a13b6e761705a700ebe85c5621c34a7f
devatulparmar <dev.atulparmar@gmail.com>
2024-03-28 20:04:21 +0530

    amazon

---
+++ lib/amazon/home.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class AmazonHome extends StatefulWidget {
+  const AmazonHome({super.key});
+
+  @override
+  State<AmazonHome> createState() => _AmazonHomeState();
+}
+
+class _AmazonHomeState extends State<AmazonHome> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: <Widget>[
+        /// Home page
+        Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.camera_alt,
+                          color: Colors.white,
+                        ),
+                        focusColor: Colors.white,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                      itemCount: 25,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Image.network(
+                          'https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg',
+                          height: 100,
+                          width: 100,
+                        ),
+                        const Text('Category'),
+                      ],
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        /// Notifications page
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: const <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// Notifications page
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: const <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// Notifications page
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: const <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// Notifications page
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: const <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ][currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.person)),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.category),
+            ),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.shopping_cart),
+            ),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.menu),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:batch5pm/amazon/home.dart';
 import 'package:batch5pm/audio_play/audio_play.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const Hello(),
+      home: const AmazonHome(),
     );
   }
 }
